4afda25f1e391a2b5acd559dfecf6fdc97a77f8c
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-06-02 22:00:42 +0600

    camp details page update info

--- src/pages/CampDetails/CampDetails.jsx
+++ src/pages/CampDetails/CampDetails.jsx
@@ -7,11 +7,12 @@ import { FaRegCalendarAlt, FaUsers } from "react-icons/fa";
 import moment from "moment";
 import { LuBadgeDollarSign } from "react-icons/lu";
 import { FaUserDoctor } from "react-icons/fa6";
+import { SiTicktick } from "react-icons/si";
 
 const CampDetails = () => {
   const axiosPublic = useAxiosPublic();
   const { id } = useParams();
-  console.log(id);
+  // console.log(id);
   const { data: camp, isLoading } = useQuery({
     queryKey: ["singleCamp"],
     queryFn: async () => {
@@ -20,7 +21,7 @@ const CampDetails = () => {
       return res.data;
     },
   });
-  console.log(camp);
+  // console.log(camp);
 
   if (isLoading) {
     return <LoadingSpinner />;
@@ -36,54 +37,98 @@ const CampDetails = () => {
     fees,
     description,
   } = camp;
+
   const date = moment(dateAndTime).format("DD/MM/YYYY, hh:mm a");
   return (
-    <div>
-      <h2 className="card-title text-[#003d6b] text-5xl mt-12 mb-6">
-        {campName}
-      </h2>
-      <div className="flex gap-6 items-center  mb-6">
-        <span className="flex gap-1 text-xl items-center text-gray-500">
-          <IoLocationOutline className=" text-gray-500" />
-          <span className="  ">{location}</span>
-        </span>{" "}
-        |
-        <span className="flex gap-1 text-xl items-center  text-gray-500 ">
-          <FaRegCalendarAlt />
-          {date}
-        </span>
-      </div>
-      <div>
-        <figure>
-          <img
-            src={image}
-            alt="Camp Img"
-            className="h-[200px] md:h-[500px] w-full object-cover"
-          />
-        </figure>
-      </div>
-      <div>
-        <div className="flex gap-1 items-center">
-          <LuBadgeDollarSign className="text-base text-[#0066b2]" />
-          <p className="text-base  text-[#0066b2]">{fees}</p>
+    <div className="w-[90%] mx-auto mt-12 ">
+      <div className="flex justify-between border-b border-b-[#003d6b99]  pb-4  mb-6">
+        <div className="flex gap-6  items-center ">
+          <span className="flex gap-2 text-base items-center text-gray-500">
+            <IoLocationOutline className=" text-gray-500" />
+            <span className="  ">{location}</span>
+          </span>{" "}
+          |
+          <span className="flex gap-2 text-base items-center  text-gray-500 ">
+            <FaRegCalendarAlt />
+            {date}
+          </span>
         </div>
-        <span className="flex gap-1 items-center text-[#0066b2]">
-          <FaUsers />
-          {participantCount}
-        </span>
-        <span className="flex gap-1 items-center text-base text-[#0066b2] ">
-          <FaRegCalendarAlt />
-          {date}
-        </span>
-        <span className="flex gap-1 items-center text-base text-[#0066b2] ">
-          <FaUserDoctor />
-          {healthcareProfessional}
-        </span>
+        <button className="btn text-[#ffffff] bg-gradient-to-br from-[#0066b2] to-[#003d6b] rounded-sm">
+          Join Camp
+        </button>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 place-items-center-start">
+        <div className="w-full ">
+          <figure>
+            <img
+              src={image}
+              alt="Camp Img"
+              className="h-[200px] md:h-[500px] w-full object-cover rounded-sm"
+            />
+          </figure>
+        </div>
+
+        <div>
+          <h2 className="card-title text-[#003d6b] text-2xl md:text-4xl mb-2">
+            {campName}
+          </h2>
+          <div className="flex  gap-2 text-[#efb312] font-medium text-2xl items-center mb-6">
+            <LuBadgeDollarSign className="" />
+            <p className="    ">
+              {fees} /{" "}
+              <span className="text-gray-400 font-normal text-xl">
+                camp fee
+              </span>
+            </p>
+          </div>
+          <div className="space-y-2">
+            <div className="flex justify-between border-b py-1">
+              <p className="font-medium">Participants</p>
+              <span className="flex gap-2 text-gray-500 items-center ">
+                <FaUsers />
+                {participantCount}
+              </span>
+            </div>
+            <div className="flex justify-between border-b py-1">
+              <p className="font-medium">Date and Time</p>
+              <span className="flex gap-2 items-center text-base text-gray-500">
+                <FaRegCalendarAlt />
+                {date}
+              </span>
+            </div>
+            <div className="flex justify-between border-b py-1">
+              <p className="font-medium">Healthcare Professional</p>
+              <span className="flex gap-2 items-center text-base text-gray-500">
+                <FaUserDoctor />
+                {healthcareProfessional}
+              </span>
+            </div>
+            <div className="flex justify-between border-b py-1">
+              <p className="font-medium">Location</p>
+              <span className="flex gap-2 text-base items-center text-gray-500">
+                <IoLocationOutline className=" text-gray-500" />
+                <span className="  ">{location}</span>
+              </span>{" "}
+            </div>
+          </div>
+          <div className="my-6">
+            <p className="font-medium mb-3">Medical Services</p>
+            {medicalServices.map((service, idx) => (
+              <p key={idx} className="flex items-center gap-3 space-y-2">
+                <SiTicktick className="text-[#efb312]" />
+                <span>{service}</span>
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="my-6">
+        <p className="text-2xl font-semibold mb-3 pb-3 border-b border-b-[#003d6b99]">
+          Description
+        </p>
         <p>{description}</p>
       </div>
-      <button className="btn text-[#ffffff] bg-gradient-to-br from-[#0066b2] to-[#003d6b] rounded-sm">
-        Join Camp
-      </button>
     </div>
   );
 };
