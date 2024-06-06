62a483c29edb5161581d5c7baf235cacf4e8c7e0
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-06-06 23:44:20 +0600

    added manage registerd camp page & functinality

--- src/pages/Dashboard/ManageRegisteredCamp/ManageRegisteredCamp.jsx
+++ src/pages/Dashboard/ManageRegisteredCamp/ManageRegisteredCamp.jsx
@@ -1,7 +1,123 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FaEdit } from "react-icons/fa";
+import { FaRegTrashCan } from "react-icons/fa6";
+import Swal from "sweetalert2";
+import useAlert from "../../../hooks/useAlert";
+import LoadingSpinner from "../../../components/LoadingSpinner/LoadingSpinner";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import moment from "moment";
+
 const ManageRegisteredCamp = () => {
+  const { user } = useAuth();
+  const [allCamps, setAllCamps] = useState([]);
+  const axiosSecure = useAxiosSecure();
+  const alert = useAlert();
+
+  const { data, isLoading, refetch } = useQuery({
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/registered-camps`);
+      setAllCamps(res.data);
+      return res.data;
+    },
+    queryKey: ["applied-data"],
+  });
+
+  const { mutateAsync } = useMutation({
+    mutationFn: async (id) => {
+      const res = await axiosSecure.delete(`/camp-delete/${id}`);
+      return res.data;
+    },
+    onSuccess: (res) => {
+      if (res.deletedCount > 0) {
+        alert("Deleted Camp!", "success");
+        refetch();
+      }
+    },
+  });
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert it!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        mutateAsync(id);
+      }
+    });
+    // const remainingJobs = data.filter();
+    // console.log(id);
+    // setAllJobs(remainingJobs);
+  };
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
   return (
-    <div>
-      <h1>Manage Registered Camp</h1>
+    <div
+      style={{ boxShadow: "2px 2px 20px #00000033" }}
+      className="w-[95%] mt-5 md:mt-10 mx-auto p-3 rounded-md"
+    >
+      <div className="w-full  text-center text-2xl md:text-3xl">
+        <p className="mb-5 font-medium">Registered Camps</p>
+      </div>
+
+      <div className=" ">
+        {/* table */}
+        <div className="overflow-x-auto mt-12">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr className="text-neutral bg-neutral-300">
+                <th>Participant Name</th>
+                <th>Camp Name</th>
+                <th>Camp Fee</th>
+                <th>Payment Status</th>
+                <th>Confirmation Status</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {allCamps.map((camp) => (
+                <tr key={camp._id}>
+                  <td>{camp.participantName}</td>
+                  <td>{camp.campName}</td>
+                  <td>{camp.fees}</td>
+
+                  <td>{camp.paymentStatus ? "Paid" : "Unpaid"}</td>
+                  <td>
+                    {camp.confirmationStatus === "confirmed"
+                      ? "Confirmed"
+                      : "Pending"}
+                  </td>
+                  <td>
+                    <div className="flex gap-3">
+                      <button
+                        disabled={camp.paymentStatus}
+                        className="disabled:text-gray-500 text-error"
+                        onClick={() => handleDelete(camp._id)}
+                      >
+                        <FaRegTrashCan
+                          className={`text-2xl   hover:rotate-180 duration-300 hover:duration-300`}
+                        />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
