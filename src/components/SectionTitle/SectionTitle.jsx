7cc34e7bd41a5e580f70ba057c6e3b4749b73f9f
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-08-25 21:13:08 +0600

    update: section title

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,14 +1,20 @@
+import { Fade } from "react-awesome-reveal";
+
 const SectionTitle = () => {
   return (
     <div>
       <div className="mt-12 md:mt-24 text-center space-y-4 md:w-1/2 mx-auto">
-        <h2 className="text-2xl md:text-4xl font-bold text-[#003d6b]">
-          Our <span className="text-[#efb312]">Popular</span> Medical Camp
-        </h2>
-        <p className="px-10 text-slate-500">
-          Discover the impactful stories from our top medical camps in
-          Bangladesh, showcasing remarkable health outcomes.
-        </p>
+        <Fade>
+          <h2 className="text-2xl md:text-4xl font-bold text-[#003d6b]">
+            Our <span className="text-[#efb312]">Popular</span> Medical Camp
+          </h2>
+        </Fade>
+        <Fade delay={200} direction="up">
+          <p className="px-10 text-slate-500">
+            Discover the impactful stories from our top medical camps in
+            Bangladesh, showcasing remarkable health outcomes.
+          </p>
+        </Fade>
       </div>
     </div>
   );
